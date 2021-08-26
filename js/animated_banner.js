cd3d9cca404aafcea3f6495b04e0f00f52a93116
Alvaro <alvarosiles499@gmail.com>
2021-08-25 21:26:21 -0600

    index-footer-credits: Iniciado (sin estilos aun)

--- js/animated_banner.js
+++
@@ -1,4 +0,0 @@
-https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/MorphSVGPlugin3.min.js
-var tl = gsap.timeline({repeat: -1, yoyo: true});
-tl.to("#banner1", {duration: 7, morphSVG: "#banner2", ease: "power1.InOut"});
-
