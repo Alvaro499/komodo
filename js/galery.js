4529e1c0cec9222b75935dbf5aabb51e65058c5b
Alvaro <alvarosiles499@gmail.com>
2021-09-03 17:56:41 -0600

    galery-moal: Reciben imagen seleccionada (falta precio y nombre)

--- js/galery.js
+++ js/galery.js
@@ -1,25 +1,37 @@
 let photos = document.querySelectorAll(".photos");
 let info = document.querySelector(".bracelet_name");
 let img_modal = document.querySelector(".img_modal");
-console.log(photos);
-
-// function getImage(){
+let modal = document.querySelector(".modal_cont");
+let close = document.querySelector(".close");
+let download_img = document.querySelector(".download_img > a");
 
     photos.forEach((element,index) => {
 
         photos[index].addEventListener("click", function(e){
-            console.log(photos[index].firstElementChild);
             let image = photos[index].children[0].attributes[0].nodeValue;
-            info.innerHTML = image;
+            let url = photos[index].children[0].attributes[0].baseURI;
+            console.log(url);
             img_modal.src = image;
-        })
+            
+                if(modal.classList.contains("modal_cont_on") === false){
         
+                    modal.classList.add("modal_cont_on");
+                    modal.classList.remove("modal_cont");
+                    document.querySelector("body").style.overflow = "hidden";
+                    download_img.href = image;
+
+                }else{
+                    modal.classList.remove("modal_cont_on");
+                    modal.classList.add("modal_cont");
+                    document.querySelector("body").style.overflow = "initial";
+                }
+        }) 
+
+        close.addEventListener("click", e =>{
+
+            if(modal.classList.contains("modal_cont_on"))
+                modal.classList.remove("modal_cont_on");
+                modal.classList.add("modal_cont");
+                document.querySelector("body").style.overflow = "initial"; 
+            })
     });
-
-// }
-
-// photos.addEventListener("click", function)
-
-
-
-// MODAL INFO
