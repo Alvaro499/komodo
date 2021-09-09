3b2bfdd1f5afdc1e28f76044a7408750dbfccd43
Alvaro <alvarosiles499@gmail.com>
2021-09-08 20:54:01 -0600

    galery-modal: Boton descarga, precio y nombre finalizados (Pendientes: Carrito, Php para la galeria, busquedas personalizadas)

--- js/galery.js
+++ js/galery.js
@@ -1,31 +1,76 @@
-let photos = document.querySelectorAll(".photos");
-let info = document.querySelector(".bracelet_name");
-let img_modal = document.querySelector(".img_modal");
 let modal = document.querySelector(".modal_cont");
 let close = document.querySelector(".close");
-let download_img = document.querySelector(".download_img > a");
+let photos = document.querySelectorAll(".photos");
+let name_img = document.querySelector(".bracelet_name > span");
+let price = document.querySelector(".bracelet_price > span");
+let download_btn = document.querySelector(".download_img");
 
+//BOTON DE DESCARGA
+
+    function downloadImage(url_img, img, name_img){
+
+        if (url_img !== null || url_img !== "") {
+            try {
+                let canvas = document.createElement("canvas");
+                document.body.appendChild(canvas);
+                canvas.width = img.width * 2;
+                canvas.height = img.height * 2;
+                canvas.getContext("2d").drawImage(img,0,0, canvas.width, canvas.height);
+
+                let pushButton = function(){
+
+                    //Soporte pata IE y EDGE (es un metodo obsoleto)
+                    if(window.navigator.msSaveBlob){
+                        window.navigator.msSaveBlob(canvas.msToBlob(), "pulsera_img.png")
+                        canvas.remove();
+                        // Soporte para resto de navegadores
+                    }else{
+                        let download_a = document.querySelector(".download_img > a")
+                        download_a.crossOrigin = "anonymous";
+                        download_a.href = canvas.toDataURL("image/jpeg");
+                        download_a.download = name_img;
+                        canvas.remove();
+                    }
+                }
+
+                download_btn.addEventListener("click", pushButton)
+            } catch (error) {
+                
+            }
+            
+            
+        }else{  
+
+        }      
+    }
+    
     photos.forEach((element,index) => {
 
         photos[index].addEventListener("click", function(e){
-            let image = photos[index].children[0].attributes[0].nodeValue;
-            let url = photos[index].children[0].attributes[0].baseURI;
-            console.log(url);
-            img_modal.src = image;
-            
-                if(modal.classList.contains("modal_cont_on") === false){
-        
-                    modal.classList.add("modal_cont_on");
-                    modal.classList.remove("modal_cont");
-                    document.querySelector("body").style.overflow = "hidden";
-                    download_img.href = image;
+            let url_img = photos[index].children[0].attributes[0].nodeValue;
+            name_img.textContent = photos[index].children[1].textContent;
+            price.textContent = photos[index].children[2].textContent;
+            let name_img_text = photos[index].children[1].textContent;
 
-                }else{
-                    modal.classList.remove("modal_cont_on");
-                    modal.classList.add("modal_cont");
-                    document.querySelector("body").style.overflow = "initial";
-                }
-        }) 
+            let img_modal = document.querySelector(".img_modal");
+            img_modal.src = url_img;
+            downloadImage(url_img,img_modal,name_img_text);
+
+            if(modal.classList.contains("modal_cont_on") === false){
+    
+                modal.classList.add("modal_cont_on");
+                modal.classList.remove("modal_cont");
+                document.querySelector("body").style.overflow = "hidden";
+               
+
+            }else{
+                modal.classList.remove("modal_cont_on");
+                modal.classList.add("modal_cont");
+                document.querySelector("body").style.overflow = "initial";
+            }
+
+
+        })
 
         close.addEventListener("click", e =>{
 
@@ -34,4 +79,4 @@ let download_img = document.querySelector(".download_img > a");
                 modal.classList.add("modal_cont");
                 document.querySelector("body").style.overflow = "initial"; 
             })
-    });
+    })
